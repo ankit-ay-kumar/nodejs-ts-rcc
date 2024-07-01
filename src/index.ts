afae3d8020525defe487e2efb4a8786695876287
Kumar <ankit.ay.kumar@capgemini.com>
2024-07-01 16:22:09 +0530

    cmt

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import app from './app';
+import  {testFun1,testFun, PI1, PI} from "nodejs-ts-rcc"
+const port = process.env.PORT || 3001;
+const asdf = testFun1(11,2);
+console.log("PI=>",PI1, asdf)
+import {rsa} from "aes-encryption-module";
+
+console.log(rsa.generateRSAKeyPair())
+console.log("coming form inner files=>",PI,testFun(11,2000))
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
